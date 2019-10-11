aafa80ab7813fd4928d0ac72de216b6c46e473e5
Devin Cooley <dcooley1350@gmail.com>
2019-10-11 08:43:20 -0700

    Scaffold project and add boilerplate

---
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorTracker.Models;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace VendorTracker.Tests
+{
+    [TestClass]
+    public class OrderTest
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+
+        }
+        [TestCleanup]
+        public void TearDown()
+        {
+            
+        }
+    }
+}
