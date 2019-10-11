891ca7e54e11c7c47f87957d127f06bfa4bb9a82
Devin Cooley <dcooley1350@gmail.com>
2019-10-11 09:11:54 -0700

    Write Order class fields and constructor

---
+++ VendorTracker/Models/Orders.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace VendorTracker.Models
+{
+    public class Order
+    {
+        public int Bread { get; set; }
+        public int Pastry { get; set; }
+        public int Id { get; }
+        private List<Order> _orders; 
+        public Order(int bread, int pastry)
+        {
+            Bread = bread;
+            Pastry = pastry;
+            _orders.Add(this);
+            Id = _orders.Count;
+        }
+    }
+}
