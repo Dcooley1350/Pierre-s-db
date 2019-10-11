675c92ebaba0daacf7076a934048a9929de6f69e
Devin Cooley <dcooley1350@gmail.com>
2019-10-11 12:46:11 -0700

    Write AddItem and Find method in vendor class, pass tests, testing complete

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -37,5 +37,27 @@ namespace VendorTracker.Tests
             List<Vendor> actualList = Vendor.GetAll();
             CollectionAssert.AreEqual(expectedList,actualList);
         }
+        [TestMethod]
+        public void Find_FindVendorById_Vendor()
+        {
+            string name = "Jarf";
+            int IdToFind = 2;
+            Vendor testVendor = new Vendor(name,venDescription);
+            Vendor foundVen = Vendor.Find(IdToFind);
+            Assert.AreEqual(testVendor,foundVen);
+        }
+        [TestMethod]
+        public void AddOrder_AssociatesOrderWitVendor_OrderList()
+        {
+            string nombre = "miguel";
+            string descriptione = "seeName";
+            string fecha = "10/10/2019";
+            int price = 32;
+            Order newOrder = new Order(nombre,descriptione,price,fecha);
+            newVendor.AddOrder(newOrder);
+            int expOrders = 1;
+            int actOrders = newVendor.Orders.Count;
+            Assert.AreEqual(expOrders,actOrders);
+        }
     }
 }
--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -8,6 +8,7 @@ namespace VendorTracker.Models
         public string VenName { get; set; }
         public string VenDescription { get; set; }
         private static List<Vendor> _allVendors = new List<Vendor> {};
+        private int Id { get; }
         public List <Order> Orders { get; set; }
 
         public Vendor(string venName, string venDescription)
@@ -15,6 +16,7 @@ namespace VendorTracker.Models
             VenName = venName;
             VenDescription = venDescription;
             _allVendors.Add(this);
+            Id = _allVendors.Count;
             Orders = new List<Order> {};
         }
         public static void ClearAll()
@@ -24,6 +26,14 @@ namespace VendorTracker.Models
         public static List<Vendor> GetAll()
         {
             return _allVendors;
+        }
+        public static Vendor Find(int Id)
+        {
+            return _allVendors[Id - 1];
+        }
+        public void AddOrder(Order newOrder)
+        {
+            Orders.Add(newOrder);
         }
     }
 }
